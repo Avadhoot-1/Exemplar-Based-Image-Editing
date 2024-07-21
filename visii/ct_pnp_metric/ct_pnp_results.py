83058cf2eac91e25fe7904aebe05726687a0654f
Ashutosh Srivastava <ashutoshsr@Ashutoshs-MacBook-Pro-2.local>
2024-07-22 01:14:46 +0530

    Add ct_pnp metric calculator

---
+++ visii/ct_pnp_metric/ct_pnp_results.py
@@ -0,0 +1,28 @@
+from s_visual import *
+# from clip_score import *
+# from directional_clip import *
+import argparse
+import json
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Resize all images in a directory to 512x512 pixels")
+    parser.add_argument("--fol", type=str, help="Path to the directory containing images")
+    parser.add_argument("--param", type=str, help="Path to the directory containing images")
+    args = parser.parse_args()
+    fol = args.fol
+    scale = args.param
+    img1 = f'/mnt/localssd/avadhoot/image_folders1/{fol}/0_0.png'
+    img2 = f'/mnt/localssd/avadhoot/image_folders1/{fol}/0_1.png'
+    img3 = f'/mnt/localssd/avadhoot/image_folders1/{fol}/1_0.png'
+    img4 = f'/mnt/localssd/avadhoot/results/{fol}/pnpclipdiff_llava_scale_{args.param}.png'
+    if not os.path.exists(img4):
+        print("Not found ", fol)
+        exit(0)
+    s_vis_score = get_s_visual(img1, img2, img3, img4)
+    data_dict = {}
+    data_dict['s_visual'] = float(s_vis_score)
+    with open(f'/mnt/localssd/avadhoot/image_folders1/{fol}/ct_pnp_{scale}.json', 'w') as file:
+        json.dump(data_dict, file)
+
+    
---
+++ visii/ct_pnp_metric/ct_pnp_results2.py
@@ -0,0 +1,48 @@
+from metrics import *
+import json
+import argparse
+from PIL import Image
+import torchvision.transforms as transforms
+import torch
+from torchmetrics.multimodal.clip_score import CLIPScore
+metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
+
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Resize all images in a directory to 512x512 pixels")
+    parser.add_argument("--fol", type=str, help="Path to the directory containing images")
+    parser.add_argument("--param", type=str, help="Path to the directory containing images")
+    args = parser.parse_args()
+    fol = args.fol
+    scale = args.param
+    img1 = f'/mnt/localssd/avadhoot/image_folders1/{fol}/0_0.png'
+    img2 = f'/mnt/localssd/avadhoot/image_folders1/{fol}/0_1.png'
+    img3 = f'/mnt/localssd/avadhoot/image_folders1/{fol}/1_0.png'
+    img4 = f'/mnt/localssd/avadhoot/results/{fol}/pnpclipdiff_llava_scale_{args.param}.png'
+    if not os.path.exists(img4):
+        exit(0)
+
+    img_fol = f'/mnt/localssd/avadhoot/image_folders1/{fol}/ins.txt'
+    with open(img_fol, 'r') as file:
+        s = file.read()
+    curr_img = Image.open(img4)
+    tensor_image = transforms.ToTensor()(curr_img)
+    tensor_image_scaled = tensor_image * 255.0
+    tensor_image_scaled = tensor_image_scaled.to(torch.uint8)
+    score = metric(tensor_image_scaled, s)
+    p = score.detach()
+    clip_s = float(p)
+    fid_val = fid_score(img3, img4)
+    lpips_val = lpips_score(img3, img4)
+    # inception_val = inception_score(args.img2)
+    ssim_val = ssim_score(img3, img4)
+
+    with open(f'/mnt/localssd/avadhoot/image_folders1/{fol}/ct_pnp_{scale}.json', 'r') as file:
+        data_dict = json.load(file)
+    data_dict['clip_s'] = clip_s
+    data_dict['fid_val'] = fid_val
+    data_dict['lpips_val'] = lpips_val
+    data_dict['ssim_val'] = ssim_val
+    with open(f'/mnt/localssd/avadhoot/image_folders1/{fol}/ct_pnp_{scale}.json', 'w') as file:
+        json.dump(data_dict, file)   
